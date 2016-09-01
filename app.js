216ac85ddb229403317a982b3d231237d4809a01
marb <marb61a@gmail.com>
2016-09-01 21:10:42 +0000

    apdated app.js and added index route

--- app.js
+++ app.js
@@ -17,4 +17,28 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
+// Error Handlers
+
+// Development Error Handler
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// Production Error Handler
+// No stacktraces will be leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
 module.exports = app;
