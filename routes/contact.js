66118318b1a60296733c2571f6734b90a6d7a657
marb <marb61a@gmail.com>
2016-09-04 17:03:12 +0000

    updated contact.js

--- routes/contact.js
+++ routes/contact.js
@@ -6,9 +6,34 @@ router.get('/', function(req, res, next){
     res.render('contact', {title : 'Contact'});
 });
 
-// Send email
+// Send Email
 router.post('send', function(req, res, next){
+    // Create a reusable Transporter
+    var transporter = nodemailer.createTransport({
+        service : 'Gmail',
+        auth : {
+            user: '',
+            pass: ''
+        }
+    });
     
+    // Email Setup
+    var mailOptions = {
+        from : '' ,
+        to : '',
+        subject : ''
+    };
+    
+    // Send
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);
+            res.redirect('/');    
+        } else {
+            console.log('' + info.response);
+            res.redirect('/');
+        }
+    });
 });
 
 module.exports = router;
